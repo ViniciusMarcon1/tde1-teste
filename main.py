a8e45f45d950048168d3fe6ecd26de3dc419321c
ViniciusMarcon1 <134452237+ViniciusMarcon1@users.noreply.github.com>
2024-08-21 21:58:59 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -2,62 +2,66 @@
 
 #OBS: MELHORAR NOME DAS VÁRIAVEIS E FUNÇÕES (PADRONIZAR) 
 
-#Extraíndo o arquivo de texto para uma váriavel 
-with open("TDE/input.txt","r") as txt:
+#Extraíndo o arquivo de texto para uma váriavel   -- ARQUIVO TXT PRECISA SER ATUALIADO DENTRO DO ARQUIVO "INPUT TXT" -- 
+with open("input.txt","r") as txt:
     raw_text = [line.strip() for line in txt.readlines()] #Tira quebra de linhas que dava problemas
 
-#Número de ops
+#Número de operações 
 op_numbers = int(raw_text[0])
 
 # Função para transformar cada elemento do conjunto em um elemento de lista - e botar nessa lista 
 def convert_to_list(conj_str):
-    elements = [element.strip() for element in conj_str.split(",")] # Divide a string depois de cada vírgula e remove espaços em branco ao redor
-    converted_elements = []
-    for element in elements:
-        converted_elements.append(element)
-    return converted_elements
+    return [element.strip() for element in conj_str.split(",")]
+
 
 #Extraíndo as operações em operador e listas (c1 e c2)
 def extractor(op_numbers):
     i = 1
-    while op_numbers > 0: #Lógica: ele continuar extraíndo e calculando até acabar o num de operações 
+    while op_numbers > 0: #continua extraíndo e calculando até acabar o num de operações 
         operator = raw_text[i]
         conj1 = convert_to_list(raw_text[i + 1])
         conj2 = convert_to_list(raw_text[i + 2])
         i += 3
         op_numbers -= 1
-        #print(f'{operator},\n{conj1},\n{conj2}')
-        calcular(operator,conj1,conj2) # chamando a função de calculo para cada grupo no txt
+        print(f'{operator}: Conjunto 1 {conj1}, conjunto 2 {conj2}. Resultado: {calcular(operator,conj1,conj2)}') # chamando a função de calculo para cada grupo no txt
 
-def calcular(operator,conjunto1,conjunto2):
+def calcular(operator,conj_A,conj_B): #Chama a função dependendo do operador
     if operator == 'U':
-        union(conjunto1,conjunto2)
+        return union(conj_A,conj_B)
     elif operator =='I':
-        intersection(conjunto1,conjunto2)
+        return intersection(conj_A,conj_B)
     elif operator == 'D':
-        diference(conjunto1,conjunto2)
+        return diference(conj_A,conj_B)
     elif operator == 'C':
-        cartesian(conjunto1,conjunto2)
+        return cartesian(conj_A,conj_B)
 
-def union(conjunto1,conjunto2):
-    union_conjuto = conjunto1
-    for i in conjunto2:
-        if i not in conjunto1:
+def union(conj_A,conj_B):
+    union_conjuto = conj_A
+    for i in conj_B:
+        if i not in conj_A:
             union_conjuto.append(i)
-    return(print(union_conjuto))
+    return union_conjuto
 
-def intersection(conjunto1,conjunto2):
-    intersection_conjunto = conjunto1
-    for i in conjunto1:
-        if conjunto1 in conjunto2:
+def intersection(conj_A,conj_B):
+    intersection_conjunto = []
+    for i in conj_A:
+        if i in conj_B:
             intersection_conjunto.append(i)
-    return(print(intersection_conjunto))
+    return intersection_conjunto
 
+def diference(conj_A,conj_B):
+    diference_conjunto = []
+    for i in conj_A:
+        if i not in conj_B:
+            diference_conjunto.append(i)
+    return diference_conjunto
 
-def diference(conjunto1,conjunto2):
-    print('a')
+def cartesian(conj_A,conj_B):
+    cartesian_conjuto = []
+    for i in conj_A:
+        for j in conj_B:
+            if (((i,j)) not in cartesian_conjuto):
+                cartesian_conjuto += [(i,j)]
+    return cartesian_conjuto
 
-def cartesian(conjunto1,conjunto2):
-    print('a')
-
-extractor(op_numbers)
+extractor(op_numbers) #Chamada da função que executa o programa
